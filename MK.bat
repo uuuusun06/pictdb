78c0861f36b736238b6586a38b6d79d32a9289c3
uuuusun06 <uuuusun06@gmail.com>
2023-11-19 14:39:46 +0800

    init

---
+++ MK.bat
@@ -0,0 +1,7 @@
+echo "# pictdb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/uuuusun06/pictdb.git
+git push -u origin main
